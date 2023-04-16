09249bd31e65a5091dea35b3ebc4f9ba7ff7e18c
DuckyProgrammer <lukas.eckert04@gmail.com>
2023-04-16 13:34:39 -0400

    Core code Created the main code for the plugin

---
+++ src/main/java/me/DuckyProgrammer/Hats/HatCommand.java
@@ -0,0 +1,30 @@
+package me.DuckyProgrammer.Hats;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HatCommand implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "You must be a player to use this command!");
+            return false;
+        }
+        if (!player.hasPermission("hats.hat")) {
+            player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+            return false;
+        }
+        if (player.getInventory().getItemInMainHand().getType().equals(Material.AIR)) {
+            player.sendMessage(ChatColor.RED + "You must be holding an item to use this command!");
+            return false;
+        }
+        player.getInventory().setHelmet(player.getInventory().getItemInMainHand());
+        player.getInventory().setItemInMainHand(null);
+        player.sendMessage(ChatColor.GREEN + "You are now wearing your item as a hat!");
+        return true;
+    }
+}
